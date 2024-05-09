72968c05de508206b8cc2f18dd53752e483c69fc
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-05-09 22:32:58 +0200

    Added pulsing circle

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,76 +5,85 @@ function LandingPage() {
     <>
       <Navbar />
 
-      <img
-        src="/assets/style-elements/header-bg-1.webp"
-        alt="style-image"
-        className="img-darken relative z-10"
-      />
+      <section>
+        <img
+          src="/assets/style-elements/header-bg-1.webp"
+          alt="style-image"
+          className="relative z-10"
+        />
 
-      <div className="overflow-hidden absolute top-[-5px] whitespace-nowrap z-0">
-        <span className="relative inline-block">
-          <img
-            src="/assets/style-elements/mountains.webp"
-            alt="mountain"
-            className="w-full inline-block"
-          />
-          <div className="absolute w-full h-full fade-opacity top-0 left-0"></div>
-        </span>
-      </div>
+        <div className="overflow-hidden absolute top-[-5px] whitespace-nowrap z-0">
+          <span className="relative inline-block">
+            <img
+              src="/assets/style-elements/mountains.webp"
+              alt="mountain"
+              className="w-full inline-block"
+            />
+            <div className="absolute w-full h-full fade-opacity top-0 left-0"></div>
+          </span>
+        </div>
 
-      <div className="overflow-hidden absolute top-[-10px] whitespace-nowrap">
-        <span className="animate-slide-left">
-          <img
-            src="/assets/style-elements/clouds/cloud-03.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+        <div className="overflow-hidden absolute top-[-10px] whitespace-nowrap">
+          <span className="animate-slide-left">
+            <img
+              src="/assets/style-elements/clouds/cloud-03.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-02.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+            <img
+              src="/assets/style-elements/clouds/cloud-02.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-03.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+            <img
+              src="/assets/style-elements/clouds/cloud-03.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-02.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
-        </span>
+            <img
+              src="/assets/style-elements/clouds/cloud-02.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
+          </span>
 
-        <span className="animate-slide-left">
-          <img
-            src="/assets/style-elements/clouds/cloud-03.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+          <span className="animate-slide-left">
+            <img
+              src="/assets/style-elements/clouds/cloud-03.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-02.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+            <img
+              src="/assets/style-elements/clouds/cloud-02.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-03.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
+            <img
+              src="/assets/style-elements/clouds/cloud-03.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
 
-          <img
-            src="/assets/style-elements/clouds/cloud-02.webp"
-            alt="cloud"
-            className="w-full cloud inline-block"
-          />
-        </span>
-      </div>
+            <img
+              src="/assets/style-elements/clouds/cloud-02.webp"
+              alt="cloud"
+              className="w-full cloud inline-block"
+            />
+          </span>
+        </div>
+
+        <div className="absolute w-full top-[230px] md:top-[170px] lg:top-[200px] xl:top-[280px] left-[50%] translate-x-[-50%] translate-y-[-1%] z-20 flex justify-center">
+          <div className="relative w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[520px] xl:h-[520px] bg-primary rounded-full">
+            <div className="top-0 left-0 absolute w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[520px] xl:h-[520px] bg-primary rounded-full animate-scale-pulse-1 "></div>
+            <div className="top-0 left-0 absolute w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[520px] xl:h-[520px] bg-primary rounded-full animate-scale-pulse-2 "></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
